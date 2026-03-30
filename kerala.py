189c83c6dd04eed5ab981d98244cb79cdff5cf3a
Rajan2810 <Rajanrudhiyal033@gmail.com>
2026-03-30 16:45:32 +0530

    Update print statement from 'Hello' to 'Goodbye'
    Add more state

--- kerala.py
+++ kerala.py
@@ -24,7 +24,8 @@ TCP_ENDPOINTS = {
     "Maharashtra_mining": {"ip": "43.205.159.137", "port": 20006},
     "MP": {"ip": "164.52.211.243", "port": 8123},
     "Goa_Emergency": {"ip": "164.100.64.247", "port": 9032},
-    "Goa_Primary": {"ip": "164.100.64.226", "port": 9031}
+    "Goa_Primary": {"ip": "164.100.64.226", "port": 9031},
+    "RJ_Primary": {"ip": "164.100.64.204", "port": 9031}
 }
 
 # HTTP endpoints for manual data sender
